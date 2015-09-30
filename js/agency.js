019a96316d723970e676cd3506a3c546b0128aa0
Jed Tan <jedtan@stanford.edu>
2015-09-30 14:31:15 -0700

    Twitter feed

--- js/agency.js
+++ js/agency.js
@@ -3,6 +3,23 @@
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+ 
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+ 
+  return t;
+}(document, "script", "twitter-wjs"));
+
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
